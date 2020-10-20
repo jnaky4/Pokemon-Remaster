681fd4df84a6fdfcf7f2e73e1adb10e13eae2e22
jnaky4 <46930694+jnaky4@users.noreply.github.com>
2020-10-20 15:03:46 -0500

    Update PokemonObject.cs

--- PokemonObject.cs
+++ PokemonObject.cs
@@ -1,3 +1,28 @@
+/*
+Pokemon Object
+    A Pokemon will be derived from a pokemon object 
+        Each pokemon generated in the game will have the following Example Attributes:
+        name: Charizard
+        pokemonImage: PokemonImages/Charizard.jpg
+        dexnum: 5
+        level: 55
+        type1: Fire
+        type2: Flying
+        Learnset{<Slash,36>,<Flamethrower,46>,<Firespin,55>}
+        LearnableTms{Mega Punch, Sword Dance, Mega Kick, Toxic, 
+            Body Slam, Take Down, Double-Edge, Hyper Beam, Submission,
+            Counter, Seismic Toss, Rage, Dragon Rage, Earthquake, 
+            Fissure, Dig, Mimic, Double Team, Reflect, Bide, Fire Blast, 
+            Swift, Skull Bash, Rest, Substitute}
+        LearnableHMs: {Cut, Strength, Fly}
+        currentMoves: {Fire Blast, Earthquake, Sword Dance, Hyper Beam}
+        EXP100: 1600000  -> this is the amount of EXP to get to lvl 100
+        currentEXP: 650245
+        
+
+*/
+
+
 public class pokemon
 {
     private pokedex name;
@@ -7,7 +32,11 @@ public class pokemon
     private type type1;
     private type type2;
     Dictionary<move, int> learnset = new Dictionary<move, int>();
-    private int EXP;
+    ArrayList learnableTms = new ArrayList();
+    ArrayList learnableHMs = new ArrayList();
+    ArrayList currentMoves = new ArrayList();
+    private int EXP100;
+    private int currentEXP;
     public pokemon(pokedex name, int level, string Imagename)
     {
         name = name;
@@ -38,7 +67,14 @@ public class player
     Dictionary<string, int> items = new Dictionary<string, int>();
 
 }
+
 public class move
 {
-
+    int name;
+    int pp;
+    int baseDamage;
+    type type;
+    bool priority;
+    //how to decide between HM, TM, Learnable move
+    //list of pokemon that can use the move
 }
