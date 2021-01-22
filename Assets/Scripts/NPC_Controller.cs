a1aeb801219ce2478e4e4c9947e75e3f6672f006
Levi <kluaythong@gmail.com>
2021-01-22 14:46:23 -0600

    Added dialog functionality

--- Assets/Scripts/NPC_Controller.cs
+++ Assets/Scripts/NPC_Controller.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPC_Controller : MonoBehaviour, Interactable
 {
-    public string Dialog;
+    public GameObject dialogBox;
+    public Text dialogText;
+    public string dialog;
+    public bool dialogActive;
 
     public void Interact()
     {
-        Debug.Log(Dialog);
+        if (dialogBox.activeInHierarchy)
+            dialogBox.SetActive(false);
+        else
+        {
+            dialogBox.SetActive(true);
+            dialogText.text = dialog;
+        }
+        Debug.Log(dialog);
     }
 }
