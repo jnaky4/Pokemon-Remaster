37c14c3b084f45e6ccbbc7a1ad160eae765d6a24
Levi <kluaythong@gmail.com>
2021-02-05 17:02:50 -0600

    Added camera transtions

---
+++ Assets/Scripts/OverworldScript/Vcam_Shift.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Vcam_Shift : MonoBehaviour
+{
+    GameObject startCam;
+    GameObject nextCam;
+    public string startString;
+    public string endString;
+
+    public void Start()
+    {
+        startCam = GameObject.Find(startString);
+        nextCam = GameObject.Find(endString);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    { 
+
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            nextCam.SetActive(true);
+            startCam.SetActive(false);
+        }
+    }
+}
