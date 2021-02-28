0ecb3b959a39c1220946c67d014f9c266f4c3756
esb7cp <esb7cp@mail.missouri.edu>
2021-02-28 17:09:34 -0600

    same bug

--- Assets/Scripts/BattleScripts/BattleSystem.cs
+++ Assets/Scripts/BattleScripts/BattleSystem.cs
@@ -763,9 +763,9 @@ namespace Pokemon
                 if (isDead)
                 {
                     bool endBattle = true;
-                    foreach (var p in GameController.playerPokemon)
+                    for(int i = 0; i < GameController.playerPokemon.Count(s => s != null); i++)
                     {
-                        if (p.current_hp > 0)
+                        if (GameController.playerPokemon[i].current_hp > 0)
                         {
                             endBattle = false;
                             break;
