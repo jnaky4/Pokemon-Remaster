09479dba132e4a9e23b957c0e40154ab539c00a0
Levi <kluaythong@gmail.com>
2021-04-07 13:36:56 -0500

    Added pokemon namespace

--- Assets/PlayerData.cs
+++ Assets/PlayerData.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerData : MonoBehaviour
+namespace Pokemon
 {
-    public static string name = "Red";
+    public class PlayerData : MonoBehaviour
+    {
+        public static string name = "Red";
 
-    public static int pokeBalls = 10;
-    public static int greatBalls = 10;
-    public static int ultraBalls = 0;
-    public static int masterBalls = 0;
+        public static int pokeBalls = 10;
+        public static int greatBalls = 10;
+        public static int ultraBalls = 0;
+        public static int masterBalls = 0;
 
-    public static bool displayPokeBalls = true;
-    public static bool displayGreatBalls = true;
-    public static bool displayUltraBalls = false;
-    public static bool displayMasterBalls = false;
+        public static bool displayPokeBalls = true;
+        public static bool displayGreatBalls = true;
+        public static bool displayUltraBalls = false;
+        public static bool displayMasterBalls = false;
+    }
 }
