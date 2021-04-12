c440bc6b7cfdba9fcfc5c42dd3d108e18233e57d
Levi <kluaythong@gmail.com>
2021-04-12 17:58:48 -0500

    continued work on starter selection

--- Assets/Scripts/OverworldScript/StarterSelection.cs
+++ Assets/Scripts/OverworldScript/StarterSelection.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Pokemon
 {
@@ -8,6 +9,7 @@ namespace Pokemon
     {
         [SerializeField] Dialog dialog;
         [SerializeField] new string name;
+        [SerializeField] int dexNum;
         [SerializeField] GameObject buttons;
 
         public void Interact(Transform initial)
@@ -15,7 +17,14 @@ namespace Pokemon
             StartCoroutine(DialogController.Instance.ShowDialog(dialog, () =>
             {
                 buttons.SetActive(true);
+                chooseStarter();
             }));
+        }
+
+        public void chooseStarter()
+        {
+            var starter = GetComponentInParent<StarterButtons>();
+            starter.name = name;
         }
     }
 }
