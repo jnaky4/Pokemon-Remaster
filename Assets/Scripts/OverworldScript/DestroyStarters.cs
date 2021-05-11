cfc8607391f57ff831092fff96bfd5e5406c398c
Levi <kluaythong@gmail.com>
2021-05-11 13:22:58 -0500

    Fixed some pallet town issues

---
+++ Assets/Scripts/OverworldScript/DestroyStarters.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pokemon
+{
+    public class DestroyStarters : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (GameController.starterChosen)
+            {
+                Destroy(this);
+            }
+    }
+    }
+}
