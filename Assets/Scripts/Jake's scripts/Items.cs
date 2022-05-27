a0f4689c7bf2e874bf09dcf5668522cf262b3965
jnaky4 <jnaky4@users.noreply.github.com>
2022-05-27 13:43:35 -0500

    Update Items.cs

--- Assets/Scripts/Jake's scripts/Items.cs
+++ Assets/Scripts/Jake's scripts/Items.cs
@@ -7,23 +7,25 @@ using System.Threading.Tasks;
 namespace Pokemon
 
 {
-    class Items
+    public class Items
     {
         public static List<Dictionary<string, object>> all_Items = new List<Dictionary<string, object>>();
         public static Dictionary<string, Items> items_dict = new Dictionary<string, Items>();
-        string name;
-        Status status_heal;
-        int restore_health;
-        int price;
-        string description;
+        public string name;
+        public Status status_heal;
+        public int restore_health;
+        public int price;
+        public string description;
+        public int count;
 
-        Items(string name, Status status_heal, int restore_health, int price, string description)
+        Items(string name, Status status_heal, int restore_health, int price, string description, int amount = 1)
         {
             this.name = name;
             this.status_heal = status_heal;
             this.restore_health = restore_health;
             this.price = price;
             this.description = description;
+            this.count = amount;
             //items_dict.Add(name, this);
         }
 
@@ -32,6 +34,7 @@ namespace Pokemon
             items_dict.Clear();
             for (var i = 0; i < Items.all_Items.Count; i++)
             {
+                
                 string name = Items.all_Items[i]["Name"].ToString();
                 Status status_heal = Status.get_status(Items.all_Items[i]["Status"].ToString());
                 int restore_health = int.Parse(Items.all_Items[i]["Health"].ToString());
@@ -40,6 +43,23 @@ namespace Pokemon
                 Items temp_item = new Items(name, status_heal, restore_health, price, description);
                 items_dict.Add(name, temp_item);
             }
+        }
+        public static Items getItem(string name)
+        {
+            return items_dict[name];
+        }
+        public void addAmount(int amount)
+        {
+            this.count += amount;
+        }
+        public void subtractAmount(int amount)
+        {
+            this.count -= amount;
+            if(count < 0) this.count = 0;
+        }
+        public void updateAmount(int amount)
+        {
+            this.count = amount;
         }
     }
 }
